c50ee827675294d64fd9544d52311e9a879991f6
abenankunim <146184401+abenankunim@users.noreply.github.com>
2023-10-01 22:40:50 +0000

    Update assignemnt 1.py

--- assignemnt 1.py
+++ assignemnt 1.py
@@ -6,18 +6,23 @@
 
 
 
- 
+#user inputing names and contact number
 full_name = input("whats your full name")
 number = input("contact number")   
 print(full_name)
 print(number)
 
+
+#retrieving cintact information
 contact_information = {full_name:number}
 for full_name, number in contact_information.items():
     print('\n', full_name,'\n', number)
 
+#searching for a contact number using full name
 print(contact_information[full_name])
 
+
+#deleting the entire contact information.
 contact_information.clear 
 print(contact_information) 
  
